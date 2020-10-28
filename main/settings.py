ae5218e3b68ff8e2a1ef916d568331a464ceec24
sergit <sergei@domain.com>
2020-10-28 17:50:09 +0300

    commit 1.0.0.7

--- main/settings.py
+++ main/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'second',
+        'USER': 'postgres',
+        'PASSWORD': '2001',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
